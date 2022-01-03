476ef3ba9466506994055d201b0acab3d4529633
Mert Bozkurtlar <mbozkurt@k1m29s03.42istanbul.com.tr>
2022-01-03 15:36:39 +0300

    First four function implemented.

---
+++ 1_libft/libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbozkurt <42istanbul.com.tr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/03 13:18:29 by mbozkurt          #+#    #+#             */
+/*   Updated: 2022/01/03 15:35:32 by mbozkurt         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdlib.h>
+
+int	ft_isalpha(char *str);
+int	ft_isdigit(char *str);
+int	ft_isalnum(char *str);
+int	ft_isascii(char *str);
+
+#endif
